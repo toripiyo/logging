fa00ba2865bb0060b60d988a51175128597e6986
toripiyo <flatworld7@gmail.com>
2014-03-10 23:46:52 +0900

    20140310
    - blush uppping calculating logic

--- routes/calculating.js
+++ routes/calculating.js
@@ -7,12 +7,35 @@ var moment = require('moment');
 
 
 exports.showresult = function(req, res){
-	console.log(req.query.days);
+	// console.log(req.query.days);
 
-	var condition = {$and:[{'day':'20140305'}, {'day':'20140306'}, {'day':'20140307'}]};
-	Record.find({$or:[{'day':'20140305'},{'day':'20140306'}]}, function(err, data) {
-		console.log(data);
+	//find checked days 
+	var days = [];
+	for(i=0; i<req.query.days.length; i++){
+		days.push({day:req.query.days[i]});
+	}
+	console.log(days);
+
+	var condition = {$or:days};
+	Record.find(condition, {'activity':1, 'code':1, 'duration':1, 'day':1, '_id':0}, function(err, data) {
+		// console.log(data);
+
+
+
+
+
+
 		res.send('Hello. Good weather!!');
-	})
+
+	});
+
+// // http://www.shamasis.net/2009/09/fast-algorithm-to-find-unique-items-in-javascript-array/
+// Array.prototype.unique = function() {
+//     var o = {}, i, l = this.length, r = [];
+//     for(i=0; i<l;i+=1) o[this[i]] = this[i];
+//     for(i in o) r.push(o[i]);
+//     return r;
+// };
+
 
 };
