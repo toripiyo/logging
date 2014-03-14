79f2442b6f4ed3482d963cffc2b7020503be5b61
toripiyo <flatworld7@gmail.com>
2014-03-14 23:38:40 +0900

    modify calculating.js file for copy and paste table page

--- routes/calculating.js
+++ routes/calculating.js
@@ -11,21 +11,25 @@ exports.showresult = function(req, res){
 
 	//find checked days 
 	var days = [];
-	for(i=0; i<req.query.days.length; i++){
-		days.push({day:req.query.days[i]});
+	var condition;
+	console.log(typeof req.query.days);
+	console.log(req.query.days);
+	console.log(req.query.days.length);
+	if (typeof req.query.days == 'string') {
+		days = req.query.days;
+		condition = {day:days};
+	} else {
+		for(i=0; i<req.query.days.length; i++){
+			days.push({day:req.query.days[i]});
+		}
+		condition = {$or:days};
 	}
 	console.log(days);
 
-	var condition = {$or:days};
-	Record.find(condition, {'activity':1, 'code':1, 'duration':1, 'day':1, '_id':0}, function(err, data) {
-		// console.log(data);
+	Record.find(condition, {'to':1, 'from':1, 'activity':1, 'code':1, 'duration':1, 'day':1, '_id':0}, function(err, data) {
+		console.log(data);
 
-
-
-
-
-
-		res.send('Hello. Good weather!!');
+		res.render('calculating', {days:JSON.stringify(days), data:data});
 
 	});
 
